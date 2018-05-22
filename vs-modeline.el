6988413bf5c61392d5e95428d7d042ba8ea67f42
Lee Machin <me@mrl.ee>
2018-05-22 20:10:24 +0100

    Add some boilerplate

---
+++ vs-modeline.el
@@ -0,0 +1,46 @@
+;;; vs-modeline.el --- a Visual Studio Code style modeline for Emacs
+
+;; Copyright (c) 2018 Lee Machin
+
+;; Author: Lee Machin <me@mrl.ee>
+;; URL: https://github.com/leemachin/vs-modeline.el
+;; Keywords: modeline
+;; Version: 1.0.0
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; A modeline that more or less looks and feels like the status bar in
+;; Visual Studio Code.
+
+;;; Usage:
+;; put (vs-modeline-enable) in your init.el to enable
+;; `vs-modeline`.
+
+;;; Credits:
+
+;; Microsoft and the VS Code developers ;)
+
+;;; Code:
+
+
+;;;###autoload
+
+(provide 'vs-modeline)
+
+;;; vs-modeline.el ends here
+
